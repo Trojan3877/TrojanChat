b0fdaa5fe76d836e17add010f75f3e5776fe2496
Corey Leath <corey22blue@hotmail.com>
2025-06-05 16:18:59 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,37 @@
-const express = require('express');
-const app = express();
-const exampleRoutes = require('./routes/exampleRoutes');
-
-// Middleware
-app.use(express.json());
-
-// Routes
-app.use('/api/example', exampleRoutes);
-
-module.exports = app;
+{
+  "name": "TrojanChatMobile",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "start": "react-native start",
+    "android": "react-native run-android",
+    "ios": "react-native run-ios",
+    "lint": "eslint ."
+  },
+  "dependencies": {
+    "@react-native-async-storage/async-storage": "^1.21.3",
+    "@react-native-community/masked-view": "^0.1.11",
+    "@react-navigation/native": "^6.1.7",
+    "@react-navigation/stack": "^6.3.17",
+    "react": "18.2.0",
+    "react-native": "0.72.3",
+    "react-native-gesture-handler": "^2.12.0",
+    "react-native-reanimated": "^3.5.0",
+    "react-native-safe-area-context": "^4.6.3",
+    "react-native-screens": "^3.22.0",
+    "socket.io-client": "^4.6.1"
+  },
+  "devDependencies": {
+    "@babel/core": "^7.24.3",
+    "@babel/runtime": "^7.24.1",
+    "@react-native-community/eslint-config": "^4.2.0",
+    "babel-jest": "^29.8.3",
+    "eslint": "^8.47.0",
+    "jest": "^29.8.3",
+    "metro-react-native-babel-preset": "^0.76.9",
+    "react-test-renderer": "18.2.0"
+  },
+  "jest": {
+    "preset": "react-native"
+  }
+}
